baf81d8ef80342d0d217d82787672c6fbf74a5e9
Thilina Ashen Gamage <ashen.workplace@gmail.com>
2024-03-06 22:30:41 +0530

    [mvn] create fresh springboot project

---
+++ ai-book-summarizer/src/main/java/com/demo/aibooksummarizer/AiBookSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.aibooksummarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBookSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiBookSummarizerApplication.class, args);
+	}
+
+}
---
+++ ai-book-summarizer/src/test/java/com/demo/aibooksummarizer/AiBookSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.aibooksummarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiBookSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
